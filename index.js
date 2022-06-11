e5b64afded071a19a42619d0aa1d1d4f8814ed5c
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-11 23:20:02 +0300

    commit tree a05756a4e5f2c8d7a6304fad375984090d80a44c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName;
+customerName='bob';
+
+const leastFavoriteCustomer="ABY";
+
+function upperCaseCustomerName(){
+customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer="not bob";
+    
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="ELY"
+}
